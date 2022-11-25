cfe298feb51c3fa54de57ad1da35e152ac88f0e9
Billy Hunter <its5am@protonmail.ch>
2022-11-26 01:58:29 +0200

    Add missing file

---
+++ cmd/hunterversed/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/blockhunters-org/hunterverse/app"
+	"github.com/blockhunters-org/hunterverse/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd(
+		"hunterverse",
+		"cosmos",
+		app.DefaultNodeHome,
+		"hunterverse",
+		app.ModuleBasics,
+		app.New,
+		// this line is used by starport scaffolding # root/arguments
+	)
+
+	rootCmd.AddCommand(cmd.AddConsumerSectionCmd(app.DefaultNodeHome))
+
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
